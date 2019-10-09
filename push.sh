c1c2b39a2fae9f2d3654d0733cf28f71f4d4aac2
richardlt <richard.le.terrier@gmail.com>
2019-10-09 18:43:47 +0200

    common: update push script and readme

--- push.sh
+++ push.sh
@@ -3,6 +3,13 @@
 if [ $# -eq 0 ]
 	then
 		echo "No name supplied"
+    exit 1
+fi
+
+if [ -z "$REPOSITORY" ]
+	then
+		echo "No repository supplied"
+    exit 1
 fi
 
 if [[ $1 == *":"* ]]
@@ -11,16 +18,16 @@ then
   TAG=`echo $1 | cut -d ':' -f2`
 else 
   NAME=$1
-  JSON=`curl --silent https://registry.hub.docker.com/v1/repositories/$USERNAME/$NAME/tags`
+  JSON=`curl --silent https://registry.hub.docker.com/v1/repositories/$REPOSITORY/$NAME/tags`
   TAG=`JSON=$JSON node tags-explorer.js $JSON`
 fi
+
+echo "Tags :$TAG and :latest will be pushed"
 
 FOLDER=$PWD/docker-$NAME
 IMAGE=`cat "$FOLDER/image"`
 
 cd $FOLDER
-
-docker login --email $EMAIL --password $PASSWORD --username $USERNAME
 
 docker tag "$IMAGE:build" "$IMAGE:$TAG"
 docker tag "$IMAGE:build" "$IMAGE:latest"
